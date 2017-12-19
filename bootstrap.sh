ca51771e0fce978a60da299f9569d327b2f3f5e4
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2017-12-19 17:23:22 -0600

    spacing

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 clt_placeholder='/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress' &&
     sudo touch "$clt_placeholder" &&
     clt_label="$(softwareupdate -l | grep -B 1 -E 'Command Line (Developer|Tools)' | awk -F'*' '/^ +\\*/ { print $2 }' | sed 's/^ *//' | tail -n 1)"
